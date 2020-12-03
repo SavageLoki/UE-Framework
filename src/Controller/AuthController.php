5657c357e0cc2e873b0e76468eceff0343df4943
trannois <harold.trannois@u-picardie.fr>
2020-12-03 10:20:03 +0100

    Traduction, personalisation du message d'erreur

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AuthController extends AbstractController
 {
@@ -28,19 +29,29 @@ class AuthController extends AbstractController
 
 
         $builder->add('nom', TextType::class,[
-            'constraints'=>[$contrainte, new Length(['min'=>2])]
+            'constraints'=>[$contrainte, new Length(['min'=>2,'minMessage'=>'Too Short only {{ value }} chars instead of {{ limit }}'])]
         ])
             ->add('btSubmit', SubmitType::class);
 
         $form = $builder->getForm();
-        $infoRendu = $form->createView();
+
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->render("authok.html.twig", ["data" => $form->getData()]);
+            return $this->forward('App\Controller\AuthController::welcome');
         } else {
+            $infoRendu = $form->createView();
             return $this->render("login.html.twig", ["infoForm" => $infoRendu]);
         }
     }
 
+    /**
+     * @Route("/welcome", Name="auth_welcome")
+     * @param TranslatorInterface $translator
+     * @return Response
+     */
+    public function welcome( TranslatorInterface $translator)
+    {
+        return new Response($translator->trans("Welcome"));
+    }
 }
